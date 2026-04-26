6e70920c12e07a839bfaf65d1b0e547b161bfb09
Maximm <urbinamax34@gmail.com>
2026-04-26 12:32:02 -0600

    agregar formularios con PHP

--- db.php
+++ db.php
@@ -4,9 +4,9 @@
 
 function conectarDB() {
     $host = "localhost";
-    $db   = "jreynoso_db";
-    $user = "jreynoso";
-    $pass = "12345678";
+    $db   = "murbina_db";
+    $user = "murbina_user";
+    $pass = "Mandarina";
     $charset = "utf8mb4";
 
     // El DSN (Data Source Name) define el tipo de driver y los datos del servidor
